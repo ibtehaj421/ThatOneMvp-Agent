0098a4432fe8a897bff46199f6799971b5da9595
Umer Farooq <umerfarooq100888@outlook.com>
2026-04-01 13:16:38 +0500

    Fixed Database Config

--- app/rag/ingest_meditod.py
+++ app/rag/ingest_meditod.py
@@ -13,7 +13,7 @@ import sys
 import re
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_postgres import PGVector
 from langchain_core.documents import Document
